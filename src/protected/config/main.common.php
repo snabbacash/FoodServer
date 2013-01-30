aeb6cab1e10aa08ad6ecd8d7609dc6869e32b4e5
Sam Stenvall <neggelandia@gmail.com>
2013-01-30 21:16:36 +0200

    Change URL format
    Use /controller/action instead of ?r=controller/action

--- src/protected/config/main.common.php
+++ src/protected/config/main.common.php
@@ -41,16 +41,15 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
 		// use tunneling
 		'db'=>array(
 			'connectionString'=>'mysql:host=localhost;port=9987;dbname=cash',
