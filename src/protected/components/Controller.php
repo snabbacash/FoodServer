17e988834e7f477724b270d7c39bf212f7fc739a
Sam Stenvall <neggelandia@gmail.com>
2013-01-31 02:34:40 +0200

    Added a JSON decoding filter to the base controller.
    This means that on POST requests the JSON payload will
    automatically appear in $_decodedJsonData and an
    exception will be thrown if the JSON is missing or
    malformed

--- src/protected/components/Controller.php
+++ src/protected/components/Controller.php
@@ -7,4 +7,55 @@
 class Controller extends CController
 {
 	
+	/**
+	 * @var mixed decoded JSON data from POST requests
+	 */
+	protected $_decodedJsonData;
+	
+	/**
+	 * Initializes the controller
+	 */
+	public function init()
+	{
+		// We will always be outputting JSON
+		header("Content-type: application/json");
+		
+		parent::init();
+	}
+	
+	/**
+	 * @return array the filters for this controller
+	 */
+	public function filters()
+	{
+		return array(
+			'decodeJsonPayload',
+		);
+	}
+	
+	/**
+	 * Decodes the "data" parameter from POST requests and stoes it in a member 
+	 * variable so that we don't have to do it in every POST action.
+	 * @param CFilterChain $filterChain the filter chain
+	 * @throws CHttpException if we have a POST request without a "data" 
+	 * parameter or if the JSON is malformed
+	 */
+	public function filterDecodeJsonPayload($filterChain)
+	{
+		if (Yii::app()->request->isPostRequest)
+		{
+			if (!isset($_POST['data']))
+				throw new CHttpException(400, 'Invalid request');
+
+			$data = CJSON::decode($_POST['data'], false);
+			if ($data === null)
+				throw new CHttpException(400, 'Malformed JSON data');
+
+			$this->_decodedJsonData = $data;
+		}
+
+		// Continue execution
+		$filterChain->run();
+	}
+
 }
--- src/protected/controllers/SiteController.php
+++ src/protected/controllers/SiteController.php
@@ -4,6 +4,17 @@ class SiteController extends Controller
 {
 
 	/**
+	 * Override parent implementation so the application can reach site/error 
+	 * even when an exception is thrown before the action is called (e.g. in 
+	 * a filter)
+	 * @return array the filters for this controllers
+	 */
+	public function filters()
+	{
+		return array();
+	}
+
+	/**
 	 * This is the default 'index' action that is invoked
 	 * when an action is not explicitly requested by users.
 	 */
